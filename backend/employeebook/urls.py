4d7eec4206cef100be0b3fbda0d6eca7d5d714d9
Alexander Rassokha <arassokha@ruscoal.ru>
2026-04-22 17:51:56 +0300

    Setup docker compose environment * Dockerfile * docker compose project * CI script

--- backend/employeebook/urls.py
+++ backend/employeebook/urls.py
@@ -1,8 +1,9 @@
 """
-URL configuration for backend project.
+URL configuration for employeebook project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,6 +15,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
