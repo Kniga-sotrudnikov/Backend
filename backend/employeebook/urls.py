d27f13144ec2ee7c3deab7314cddfa39c24cd426
Влад Тимофеев <vladtimofeev5@yandex.ru>
2026-05-07 13:55:58 +0400

    Обновил requirements.txt

--- backend/employeebook/urls.py
+++ backend/employeebook/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
