b72ed590dafd665d5b1cb2cc71989876b7c95310
Влад Тимофеев <vladtimofeev5@yandex.ru>
2026-06-11 21:48:51 +0400

    Fixed LANGUAGE_CODE, made built-in and custom errors in Russian

--- backend/employeebook/settings.py
+++ backend/employeebook/settings.py
@@ -148,7 +148,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 LOCALE_PATHS = [str(BASE_DIR / 'locale') if DEBUG else '/var/www/django/locale']
 
