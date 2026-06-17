1049626860f6e393c53f84e4d96225054dae1582
Vlad <149313543+Dospoya@users.noreply.github.com>
2026-06-17 12:03:48 +0300

    Update tasks.py

--- backend/notifications/tasks.py
+++ backend/notifications/tasks.py
@@ -1,7 +1,6 @@
 import logging
 
 from celery import shared_task
-
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
